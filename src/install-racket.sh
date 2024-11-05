ec946b03dc48214f2a4e4086f25c8f6e8326db07
cjpopova <casparpopova2023@u.northwestern.edu>
2024-11-05 11:50:31 -0500

    Add cmd line arg and newline

--- src/install-racket.sh
+++ src/install-racket.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-VERSION=$1
+VARIANT=$1
+VERSION=$2
 
-INSTALLER="racket-${VERSION}-x86_64-linux-cs.sh"
+INSTALLER="racket-${VERSION}-x86_64-linux-${VARIANT,,}.sh" # variant must be converted to lowercase
 wget "https://mirror.racket-lang.org/installers/${VERSION}/$INSTALLER"
 chmod u+x ./$INSTALLER
 ./$INSTALLER <<EOF
 no
 1
+
+# there MUST be a newline after the previous command to finish the racket installation
 export PATH="/usr/racket/bin:$PATH"
