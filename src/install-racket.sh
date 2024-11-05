d14f7d2cabad303ad5b1db1ce27dc56e08dc10d5
cjpopova <casparpopova2023@u.northwestern.edu>
2024-11-05 11:30:17 -0500

    Attempt cmd line args and PATH

--- src/install-racket.sh
+++ src/install-racket.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-VERSION="8.6"
+VERSION=$1
 
 INSTALLER="racket-${VERSION}-x86_64-linux-cs.sh"
 wget "https://mirror.racket-lang.org/installers/${VERSION}/$INSTALLER"
 chmod u+x ./$INSTALLER
 ./$INSTALLER <<EOF
 no
-4
+1
+export PATH="/usr/racket/bin:$PATH"
